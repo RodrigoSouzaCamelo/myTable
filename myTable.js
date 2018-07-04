9b47b6ec7718b2d13021fcd8618e3f3ab3a1925b
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2018-07-04 20:27:34 -0300

    code organization and change in style

--- myTable.js
+++ myTable.js
@@ -5,34 +5,39 @@ var pager;
 var currentPage = 0;
 var pageLength;
 var rowNum = 5;
+var backPage;
+var nextPage;
 
 function myTable(foundation) {
     this.foundation = foundation;
     this.pageLength = Math.ceil(foundation.data.length / rowNum);
     this.pager = document.getElementById(foundation.pager);
+    this.backPage = document.createElement('a');
+    this.nextPage = document.createElement('a');
+    this.table = document.createElement("table");
+    this.header = document.createElement("thead");
+    this.body = document.createElement("tbody");
+
     this.pager.className = "pager";
+    this.table.className = 'myTable';
 
-    table = document.createElement("table");
-    header = document.createElement("thead");
-    body = document.createElement("tbody");
-    table.className = 'myTable';
+    constructTHead(foundation.colNames);
+    constructTBody(foundation.data, foundation.colNames, foundation.tableDiv);
 
-    constructHead(foundation.colNames);
-    // constructBody(foundation.data, foundation.colNames);
-    Pagination(foundation.data, foundation.colNames, foundation.tableDiv);
-
-    table.appendChild(header);
-    table.appendChild(body);
-    document.getElementById(foundation.tableDiv).appendChild(table);
-
-
-    var backPage = document.createElement('a');
     backPage.innerText = "<<";
     backPage.addEventListener("click", function () {
         currentPage--;
         alterPagerEvent();
-        Pagination(data, foundation.colNames, foundation.tableDiv);
+        constructTBody(data, foundation.colNames, foundation.tableDiv);
     });
+
+    nextPage.innerText = '>>';
+    nextPage.addEventListener("click", function () {
+        currentPage++;
+        alterPagerEvent();
+        constructTBody(data, foundation.colNames, foundation.tableDiv);
+    });
+    
     this.pager.appendChild(backPage);
     for (i = 0; i < this.pageLength; i++) {
         var pagerNum = document.createElement('a');
@@ -41,17 +46,36 @@ function myTable(foundation) {
         pagerNum.className = i == this.currentPage ? 'active' : '';
         document.getElementById("pager").appendChild(pagerNum);
     }
-    var nextPage = document.createElement('a');
-    nextPage.innerText = '>>';
-    nextPage.addEventListener("click", function () {
-        currentPage++;
-        alterPagerEvent();
-        Pagination(data, foundation.colNames, foundation.tableDiv);
-    });
     this.pager.appendChild(nextPage);
+    
+    this.table.appendChild(header);
+    this.table.appendChild(body);
+    document.getElementById(foundation.tableDiv).appendChild(this.table);
 }
 
-function Pagination(data, colNames, tableDiv) {
+
+
+function clearRows(tableDiv) {
+    var div = document.getElementById(tableDiv);
+    var tbody = div.querySelector("tbody");
+    if (tbody == null)
+        return;
+
+    body = document.createElement("tbody");
+    tbody.remove();
+}
+
+function constructTHead(colNames) {
+    var row = document.createElement("tr");
+    for (i = 0; i < colNames.length; i++) {
+        var col = document.createElement("th");
+        col.innerText = colNames[i].name;
+        row.appendChild(col);
+        this.header.appendChild(row);
+    }
+}
+
+function constructTBody(data, colNames, tableDiv) {
     clearRows(tableDiv);
     for (var x = currentPage * rowNum; x < data.length && x < (currentPage + 1) * rowNum; x++) {
         var row = document.createElement("tr");
@@ -64,40 +88,6 @@ function Pagination(data, colNames, tableDiv) {
         body.appendChild(row);
         table.appendChild(body);
     }
-}
-
-function clearRows(tableDiv) {
-    var div = document.getElementById(tableDiv);
-    var tbody = div.querySelector("tbody");
-    if (tbody == null)
-        return;
-
-    body = document.createElement("tbody");
-    tbody.remove();
-}
-
-function constructHead(colNames) {
-    var row = document.createElement("tr");
-    for (i = 0; i < colNames.length; i++) {
-        var col = document.createElement("th");
-        col.innerText = colNames[i].name;
-        row.appendChild(col);
-        header.appendChild(row);
-    }
-}
-
-function constructBody(data, colNames) {
-    for (x = 0; x < data.length; x++) {
-        var row = document.createElement("tr");
-        for (y = 0; y < colNames.length; y++) {
-            var col = document.createElement("th");
-            col.innerText = data[x][colNames[y].index];
-            row.appendChild(col);
-        }
-
-        body.appendChild(row);
-    }
-
 }
 
 function alterPagerEvent() {
